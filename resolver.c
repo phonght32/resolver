b37e0e10be2b11fd2c36f6f470adb8c4a12bebfb
phonght32 <thanhphongho1998@gmail.com>
2023-12-16 21:34:37 +0700

    add basic functions

---
+++ resolver.c
@@ -0,0 +1,133 @@
+#include "stdlib.h"
+#include "resolver.h"
+
+typedef struct resolver {
+	uint32_t 					max_reload;				/*!< Max reload value */
+	uint8_t  					is_run; 				/*!< Running status */
+	resolver_func_start 		start;					/*!< Function start resolver */
+	resolver_func_stop 			stop;					/*!< Function stop resolver */
+	resolver_func_set_counter 	set_counter;			/*!< Function set counter */
+	resolver_func_get_counter 	get_counter;			/*!< Function get counter */
+} resolver_t;
+
+resolver_handle_t resolver_init(void)
+{
+	resolver_handle_t handle = calloc(1, sizeof(resolver_t));
+	if (handle == NULL)
+	{
+		return NULL;
+	}
+
+	return handle;
+}
+
+err_code_t resolver_set_config(resolver_handle_t handle, resolver_cfg_t cfg)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	handle->max_reload = cfg.max_reload;
+	handle->is_run = 0;
+	handle->start = cfg.start;
+	handle->stop = cfg.stop;
+	handle->set_counter = cfg.set_counter;
+	handle->get_counter = cfg.get_counter;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t resolver_config(resolver_handle_t handle)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	/* Nothing to do */
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t resolver_start(resolver_handle_t handle)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	err_code_t err;
+
+	err = handle->start();
+	if (err != ERR_CODE_SUCCESS)
+	{
+		return err;
+	}
+
+	handle->is_run = 1;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t resolver_stop(resolver_handle_t handle)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	err_code_t err;
+
+	err = handle->stop();
+	if (err != ERR_CODE_SUCCESS)
+	{
+		return err;
+	}
+
+	handle->is_run = 0;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t resolver_get_value(resolver_handle_t handle, uint32_t *value)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	err_code_t err;
+
+	err = handle->get_counter(value);
+	if (err != ERR_CODE_SUCCESS)
+	{
+		return err;
+	}
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t resolver_set_value(resolver_handle_t handle, uint32_t value)
+{
+	/* Check if handle structure is NULL */
+	if (handle == NULL)
+	{
+		return ERR_CODE_NULL_PTR;
+	}
+
+	err_code_t err;
+
+	err = handle->set_counter(value);
+	if (err != ERR_CODE_SUCCESS)
+	{
+		return err;
+	}
+
+	return ERR_CODE_SUCCESS;
+}
---
+++ resolver.h
@@ -0,0 +1,134 @@
+// MIT License
+
+// Copyright (c) 2023 phonght32
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef __RESOLVER_H__
+#define __RESOLVER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "err_code.h"
+
+typedef err_code_t (*resolver_func_start)(void);
+typedef err_code_t (*resolver_func_stop)(void);
+typedef err_code_t (*resolver_func_set_counter)(uint32_t value);
+typedef err_code_t (*resolver_func_get_counter)(uint32_t *value);
+
+typedef struct resolver *resolver_handle_t;
+
+typedef struct {
+	uint32_t 					max_reload;             /*!< Max reload value */
+	resolver_func_start 		start;					/*!< Function start resolver */
+	resolver_func_stop 			stop;					/*!< Function stop resolver */
+	resolver_func_set_counter 	set_counter;			/*!< Function set counter */
+	resolver_func_get_counter 	get_counter;			/*!< Function get counter */
+} resolver_cfg_t;
+
+/*
+ * @brief   Initialize resolver with default parameters.
+ *
+ * @note    This function must be called first.
+ *
+ * @param   None.
+ *
+ * @return
+ *      - Handle structure: Success.
+ *      - Others:           Fail.
+ */
+resolver_handle_t resolver_init(void);
+
+/*
+ * @brief   Set configuration parameters.
+ *
+ * @param 	handle Handle structure.
+ * @param   cfg Configuration structure.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t resolver_set_config(resolver_handle_t handle, resolver_cfg_t cfg);
+
+/*
+ * @brief   Configure resolver to run.
+ *
+ * @param 	handle Handle structure.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t resolver_config(resolver_handle_t handle);
+
+/*
+ * @brief   Start resolver.
+ *
+ * @param   handle Handle structure.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t resolver_start(resolver_handle_t handle);
+
+/*
+ * @brief   Stop resolver.
+ *
+ * @param   handle Handle structure.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t resolver_stop(resolver_handle_t handle);
+
+/*
+ * @brief   Get resolver counter value.
+ *
+ * @param   handle Handle structure.
+ * @param   value Counter value.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t resolver_get_value(resolver_handle_t handle, uint32_t *value);
+
+/*
+ * @brief   Set resolver counter value.
+ *
+ * @param   handle Handle structure.
+ * @param   value Counter value.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS: Success.
+ *      - Others:           Fail.
+ */
+err_code_t resolver_set_value(resolver_handle_t handle, uint32_t value);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RESOLVER_H__ */
